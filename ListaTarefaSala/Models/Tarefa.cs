c5b27fcbf177d79973e36f82927b9c6dd468ef10
felipe <FelipeCorreaLemos@WIN-D8I71GMGMD7>
2026-05-18 10:07:51 -0300

    .............

--- ListaTarefaSala/Models/Tarefa.cs
+++ ListaTarefaSala/Models/Tarefa.cs
@@ -5,5 +5,15 @@ public class Tarefa
     public string Titulo = "";
     public string Descricao = "";
     public bool Concluida;
+
+    private string caminhoTarefa1 = "tarefa1.json";
+
+    public void CadastrarTarefa(string titulo, string descricao)
+    {
+        Tarefa tarefa = new Tarefa();
+        tarefa.Titulo = titulo;
+        tarefa.Descricao = descricao;
+        tarefa.Concluida = false;
+    }
 }
 
